5b5eb2915240280060222a221e0f78831290fa95
exactone <d04921005@ntu.edu.tw>
2017-06-28 04:33:52 +0800

    Update gcp_web_init.sh

--- gcp_web_init.sh
+++ gcp_web_init.sh
@@ -19,12 +19,12 @@ sudo apt-get install -y htop
 sudo apt-get install -y p7zip-full
 
 
-echo '=============='
-echo 'install cuda'
-echo '=============='
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo yes y | dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo apt-get update && sudo apt-get install cuda
+#echo '=============='
+#echo 'install cuda'
+#echo '=============='
+#wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+#sudo yes y | dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+#sudo apt-get update && sudo apt-get install cuda
 
 
 echo '=============='
@@ -42,16 +42,21 @@ sudo apt-get install gcc-4.8 g++-4.8 -y && \
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8;
 
 
+#echo '=============='
+#echo 'install CUDA, cuDNN'
+#echo '=============='
+#sudo cp gcp_script/gdrive-linux-x64 /usr/local/bin/gdrive
+#sudo chmod a+x /usr/local/bin/gdrive
+#cd ~/download
+##wget https://drive.google.com/open?id=0B3slvjD82cAJQncwMVJWc0oxNlk -O cudnn-8.0-linux-x64-v5.1.tar
+#gdrive download 0B3slvjD82cAJQncwMVJWc0oxNlk
+#tar -xf cudnn-8.0-linux-x64-v5.1.tar
+#cd ~
+
 echo '=============='
-echo 'install CUDA, cuDNN'
+echo 'install apach'
 echo '=============='
-sudo cp gcp_script/gdrive-linux-x64 /usr/local/bin/gdrive
-sudo chmod a+x /usr/local/bin/gdrive
-cd ~/download
-#wget https://drive.google.com/open?id=0B3slvjD82cAJQncwMVJWc0oxNlk -O cudnn-8.0-linux-x64-v5.1.tar
-gdrive download 0B3slvjD82cAJQncwMVJWc0oxNlk
-tar -xf cudnn-8.0-linux-x64-v5.1.tar
-cd ~
+sudo apt-get install -y apache2
 
 
 echo '=============='
@@ -95,51 +100,63 @@ sed -i "s/#c.NotebookApp.port = 8888/c.NotebookApp.port = 9999/g" ~/.jupyter/jup
 
 
 echo '=============='
-echo 'install keras, keras-tqdm, tensorflow-gpu'
+echo 'install Django'
 echo '=============='
-conda create --name keras2.0 --clone root
-source activate keras2.0
-pip install keras
-# https://github.com/bstriner/keras-tqdm
-pip install keras-tqdm
-pip install tensorflow-gpu
+conda create --name Django --clone root
+source activate Django
+pip install Django
+source deactivate
+## https://github.com/bstriner/keras-tqdm
+#pip install keras-tqdm
+#pip install tensorflow-gpu
 
 
-echo '=============='
-echo 'install kaggle-cli, '
-echo '=============='
-# https://github.com/floydwch/kaggle-cli
-pip install kaggle-cli
+#echo '=============='
+#echo 'install keras, keras-tqdm, tensorflow-gpu'
+#echo '=============='
+#conda create --name keras2.0 --clone root
+#source activate keras2.0
+#pip install keras
+## https://github.com/bstriner/keras-tqdm
+#pip install keras-tqdm
+#pip install tensorflow-gpu
 
 
-echo '=============='
-echo 'install gensim, word2vec, nltk'
-echo '=============='
-pip install gensim
-pip install word2vec
-pip install nltk
-touch nltk_download.py
-echo "import nltk" >> nltk_download.py
-echo "nltk.download('all')" >> nltk_download.py
-python nltk_download.py
-rm -f nltk_download.py
+#echo '=============='
+#echo 'install kaggle-cli, '
+#echo '=============='
+## https://github.com/floydwch/kaggle-cli
+#pip install kaggle-cli
 
 
-echo '=============='
-echo 'install xgboost'
-echo '=============='
-# https://github.com/dmlc/xgboost
-# https://pypi.python.org/pypi/xgboost/
-pip install xgboost
-source activate keras2.0
-# solve 'GOMP_4.0 not found' bug
-# https://github.com/dmlc/xgboost/issues/1786
-conda install libgcc
-# solve 'libstdc++.so.6: version `GLIBCXX_3.4.22' not found' bug
-# https://itbilu.com/linux/management/NymXRUieg.html
-cd ~/anaconda3/envs/keras2.0/lib
-cp /usr/lib32/libstdc++.so.6.0.23 .
-sudo rm -rf libstdc++.so.6
-sudo ln -s libstdc++.so.6.0.23 libstdc++.so.6
-cd ~
-source deactivate keras2.0
+#echo '=============='
+#echo 'install gensim, word2vec, nltk'
+#echo '=============='
+#pip install gensim
+#pip install word2vec
+#pip install nltk
+#touch nltk_download.py
+#echo "import nltk" >> nltk_download.py
+#echo "nltk.download('all')" >> nltk_download.py
+#python nltk_download.py
+#rm -f nltk_download.py
+
+
+#echo '=============='
+#echo 'install xgboost'
+#echo '=============='
+## https://github.com/dmlc/xgboost
+## https://pypi.python.org/pypi/xgboost/
+#pip install xgboost
+#source activate keras2.0
+## solve 'GOMP_4.0 not found' bug
+## https://github.com/dmlc/xgboost/issues/1786
+#conda install libgcc
+## solve 'libstdc++.so.6: version `GLIBCXX_3.4.22' not found' bug
+## https://itbilu.com/linux/management/NymXRUieg.html
+#cd ~/anaconda3/envs/keras2.0/lib
+#cp /usr/lib32/libstdc++.so.6.0.23 .
+#sudo rm -rf libstdc++.so.6
+#sudo ln -s libstdc++.so.6.0.23 libstdc++.so.6
+#cd ~
+#source deactivate keras2.0
