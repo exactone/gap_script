b27d069daa1f969163b07bb1cf2181117005b6af
exactone <d04921005@ntu.edu.tw>
2017-10-25 01:38:15 +0800

    Update gcp_init.sh
    no install gcc 6.0 gensim, word2vec, nltk, xgboost

--- gcp_init.sh
+++ gcp_init.sh
@@ -57,25 +57,25 @@ export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
 
 
-echo '=============='
-echo 'install gcc'
-echo '=============='
-sudo apt-get update && \
-sudo apt-get install build-essential software-properties-common -y && \
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
-sudo apt-get update && \
-sudo apt-get install gcc-snapshot -y && \
-sudo apt-get update && \
-sudo apt-get install gcc-6 g++-6 -y && \
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6 && \
-sudo apt-get install gcc-4.8 g++-4.8 -y && \
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8;
+#echo '=============='
+#echo 'install gcc'
+#echo '=============='
+#sudo apt-get update && \
+#sudo apt-get install build-essential software-properties-common -y && \
+#sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
+#sudo apt-get update && \
+#sudo apt-get install gcc-snapshot -y && \
+#sudo apt-get update && \
+#sudo apt-get install gcc-6 g++-6 -y && \
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6 && \
+#sudo apt-get install gcc-4.8 g++-4.8 -y && \
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8;
 
 
 echo '=============='
 echo 'install CUDA, cuDNN'
 echo '=============='
-sudo cp gcp_script/gdrive-linux-x64 /usr/local/bin/gdrive
+sudo cp ~/gcp_script/gdrive-linux-x64 /usr/local/bin/gdrive
 sudo chmod a+x /usr/local/bin/gdrive
 cd ~/download
 #wget https://drive.google.com/open?id=0B3slvjD82cAJX05EdzVQUkZjdzQ -O cudnn-8.0-linux-x64-v5.1.tar
@@ -151,34 +151,34 @@ echo '=============='
 pip install kaggle-cli
 
 
-echo '=============='
-echo 'install gensim, word2vec, nltk'
-echo '=============='
-pip install gensim
-pip install word2vec
-pip install nltk
-touch nltk_download.py
-echo "import nltk" >> nltk_download.py
-echo "nltk.download('all')" >> nltk_download.py
-python nltk_download.py
-rm -f nltk_download.py
+#echo '=============='
+#echo 'install gensim, word2vec, nltk'
+#echo '=============='
+#pip install gensim
+#pip install word2vec
+#pip install nltk
+#touch nltk_download.py
+#echo "import nltk" >> nltk_download.py
+#echo "nltk.download('all')" >> nltk_download.py
+#python nltk_download.py
+#rm -f nltk_download.py
 
 
-echo '=============='
-echo 'install xgboost'
-echo '=============='
-# https://github.com/dmlc/xgboost
-# https://pypi.python.org/pypi/xgboost/
-pip install xgboost
-source activate keras2.0
-# solve 'GOMP_4.0 not found' bug
-# https://github.com/dmlc/xgboost/issues/1786
-conda install libgcc
-# solve 'libstdc++.so.6: version `GLIBCXX_3.4.22' not found' bug
-# https://itbilu.com/linux/management/NymXRUieg.html
-cd ~/anaconda3/envs/keras2.0/lib
-cp /usr/lib32/libstdc++.so.6.0.23 .
-sudo rm -rf libstdc++.so.6
-sudo ln -s libstdc++.so.6.0.23 libstdc++.so.6
-cd ~
-source deactivate keras2.0
+#echo '=============='
+#echo 'install xgboost'
+#echo '=============='
+## https://github.com/dmlc/xgboost
+## https://pypi.python.org/pypi/xgboost/
+#pip install xgboost
+#source activate keras2.0
+## solve 'GOMP_4.0 not found' bug
+## https://github.com/dmlc/xgboost/issues/1786
+#conda install libgcc
+## solve 'libstdc++.so.6: version `GLIBCXX_3.4.22' not found' bug
+## https://itbilu.com/linux/management/NymXRUieg.html
+#cd ~/anaconda3/envs/keras2.0/lib
+#cp /usr/lib32/libstdc++.so.6.0.23 .
+#sudo rm -rf libstdc++.so.6
+#sudo ln -s libstdc++.so.6.0.23 libstdc++.so.6
+#cd ~
+#source deactivate keras2.0
